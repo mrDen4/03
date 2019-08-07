7fd0657a2dec85213884319db2b7de0b18d6a4b8
Dennis <mr.den.saint@yandex.ru>
2019-08-07 11:11:14 +0300

    add adaptive for sales page

--- src/js/main.js
+++ src/js/main.js
@@ -1,103 +1,119 @@
 $(document).ready(function () {
-    //Слайдер для лицензий на главной странице
-    $('.license .license__slider .slider__slides').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 3,
-        adaptiveHeight: true,
-        autoplay: true,
-        arrows: true,
-        slidesToScroll: 1,
-        prevArrow: $('.license .license__slider .slider__arrows .arrows__arrow--left'),
-        nextArrow: $('.license .license__slider .slider__arrows .arrows__arrow--right'),
-        responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToScroll: 3,
-                slidesToShow: 3
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToScroll: 2,
-                slidesToShow: 2
-              }
-            }
-        ]
-    });
+  //Слайдер для лицензий на главной странице
+  $('.license .license__slider .slider__slides').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    adaptiveHeight: true,
+    autoplay: true,
+    arrows: true,
+    slidesToScroll: 1,
+    prevArrow: $('.license .license__slider .slider__arrows .arrows__arrow--left'),
+    nextArrow: $('.license .license__slider .slider__arrows .arrows__arrow--right'),
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToScroll: 3,
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToScroll: 2,
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
 
-    //Слайдер для отзывов на главной странице
-    $('.reviews .license__slider .slider__slides').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 3,
-        adaptiveHeight: true,
-        autoplay: true,
-        arrows: true,
-        slidesToScroll: 1,
-        prevArrow: $('.reviews .license__slider .slider__arrows .arrows__arrow--left'),
-        nextArrow: $('.reviews .license__slider .slider__arrows .arrows__arrow--right'),
-        responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                slidesToScroll: 3,
-                slidesToShow: 3
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToScroll: 2,
-                slidesToShow: 2
-              }
-            }
-        ]
-    });
+  //Слайдер для отзывов на главной странице
+  $('.reviews .license__slider .slider__slides').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    adaptiveHeight: true,
+    autoplay: true,
+    arrows: true,
+    slidesToScroll: 1,
+    prevArrow: $('.reviews .license__slider .slider__arrows .arrows__arrow--left'),
+    nextArrow: $('.reviews .license__slider .slider__arrows .arrows__arrow--right'),
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToScroll: 3,
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToScroll: 2,
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
 
-    //Слайдер для страницы с акциями
-    $('.sales .sales__list').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 2,
-        adaptiveHeight: true,
-        autoplay: true,
-        arrows: true,
-        slidesToScroll: 1,
-        prevArrow: $('.sales .arrows__arrow--left'),
-        nextArrow: $('.sales .arrows__arrow--right')
-    });
+  //Слайдер для страницы с акциями
+  $('.sales .sales__list').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    adaptiveHeight: true,
+    autoplay: false,
+    arrows: true,
+    slidesToScroll: 1,
+    prevArrow: $('.sales .arrows__arrow--left'),
+    nextArrow: $('.sales .arrows__arrow--right'),
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 
-    //Кнопка "показать еще" на странице с Услугой в блоке с прайсом
-    $('.service-price .service-price__btn').on('click', function () {
-        $('.service-price .service-price__list .list__item--hide').removeClass('list__item--hide');
-        $(this).addClass('service-price__btn--hide');
-    });
+  //Кнопка "показать еще" на странице с Услугой в блоке с прайсом
+  $('.service-price .service-price__btn').on('click', function () {
+    $('.service-price .service-price__list .list__item--hide').removeClass('list__item--hide');
+    $(this).addClass('service-price__btn--hide');
+  });
 
-    //Кнопка "показать еще" на страницу с Услугами
-    $('.services .services__btn').on('click', function () {
-        $('.services .services__list .list__item--hide').removeClass('list__item--hide');
-        $(this).addClass('services__btn--hide');
-    });
+  //Кнопка "показать еще" на страницу с Услугами
+  $('.services .services__btn').on('click', function () {
+    $('.services .services__list .list__item--hide').removeClass('list__item--hide');
+    $(this).addClass('services__btn--hide');
+  });
 
-    //popup с обратным звонком
-    $('.hero .btns__btn').on('click', function () {
-        $('.popup').addClass('popup--active');
-        $('.bg').addClass('bg--active');
-    });
+  //popup с обратным звонком
+  $('.hero .btns__btn').on('click', function () {
+    $('.popup').addClass('popup--active');
+    $('.bg').addClass('bg--active');
+  });
 
-    $('.popup .popup__exit').on('click', function () {
-        $('.bg').removeClass('bg--active');
-        $('.popup').removeClass('popup--active');
-    });
+  $('.popup .popup__exit').on('click', function () {
+    $('.bg').removeClass('bg--active');
+    $('.popup').removeClass('popup--active');
+  });
 
-    $('.bg').on('click', function () {
-        $('.bg').removeClass('bg--active');
-        $('.popup').removeClass('popup--active');
-    });
+  $('.bg').on('click', function () {
+    $('.bg').removeClass('bg--active');
+    $('.popup').removeClass('popup--active');
+  });
 });
